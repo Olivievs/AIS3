c15e83fd0476ace8afe9806f4f79e43ff0977e64
Olivievs <mie2000@yandex.ru>
2023-06-02 01:15:49 +0300

    upd

--- dfs.py
+++ dfs.py
@@ -1,17 +1,28 @@
-def dfs(graph, source,path = []):
-       if source not in path:
-           path.append(source)
-           if source not in graph:
+def dfs(graph, start, finish, len, path=[]):
+       if start not in path:
+           path.append(start)
+           len=len+1
+           if start==finish:
                return path
-           for neighbour in graph[source]:
-               path = dfs(graph, neighbour, path)
-       return path
+           for neighbour in graph[start]:
+               path = dfs(graph, start, finish, len, path)
+       return [path, len]
 
-graph = {"A":["B","C", "D"],
-           "B":["E"],
-           "C":["F","G"],
-           "D":["H"],
-           "E":["I"],
-           "F":["J"]}
-path = dfs(graph, "A")
-print(" ".join(path))
+def init_graph():
+	dict = {}
+	am = input("Введите количество вершин\n")
+	for i in range(1, int(am)+1):
+		cur_res = []
+		am_of_val = input(f"Сколько вершин связывает вершину {i}?\n")
+		for j in range(1, int(am_of_val)+1):
+			cur_res.append(int(input(f"Введите вершину {j}\n")))
+		dict[i]=cur_res
+	return dict
+
+graph = init_graph()
+print(graph)
+path, len = dfs(graph, 2, 4, 0)
+print(path)
+print(len)
+
+
